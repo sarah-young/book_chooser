b771049cd3827ab7fcee0d76edb997c43da29903
Steven Schlotterbeck <StevenSchlotterbeck@gmail.com>
2019-12-13 17:08:38 -0800

    OCD

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 Red Umbrella Book Club's Book Chooser (2020)
 ============================================
 
-### The Problem
+## The Problem
 
 Reading is awesome, but choosing a book can be tough. There are so many authors, so many books. Analysis paralysis abound!
 
 Red Umbrella Book Club's Book Chooser aims to solve that problem. Using The 2020 Tournament of Books from The Morning News and Python's Beautiful Soup, the Book Chooser chooses a book for you.
 
-### How to Install
+## How to Install
+
 (1) Git Clone!
 Go to GitHub Clone the repo into while in your directory. GitHub Clone
 `git clone <repo_name>`
@@ -22,13 +23,13 @@ Create: `python3 -m venv env`
 
 Activate: `source env/bin/activate`
 
-More information: https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+More information: <https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/>
 
 (3) Pip Install
-If you've already downloaded Python 2 or 3, you have pip installed on your computer.
-`pip install -r requirements.txt`
+If you've already downloaded Python 2 or 3, you have pip installed on your computer. `pip install -r requirements.txt`
 
-### How to Run
-Start the book chooser by using the command `python tournament_of_books.py` in your terminal from your cloned repo.
+## How to Run
+
+Start the book chooser by using the command `python tournamentofbooks.py` in your terminal from your cloned repo.
 
 Happy reading!
--- tournamentofbooks.py
+++ tournamentofbooks.py
@@ -3,9 +3,13 @@ import requests
 from random import randrange
 import time
 
-def random_book(books, number_of_books):
-    "Chooses a random book from a list."
-    _index = randrange(0, number_of_books-1)
+library_uri = "http://themorningnews.org/article/the-year-in-fiction-2019"
+
+def random_book(books, number_of_books, previous_pick = -1):
+    """Chooses a random book from a list."""
+    _index = randrange(0, number_of_books - 1)
+    while _index == previous_pick:
+        _index = randrange(0, number_of_books - 1)
     print()
     print()
     print("How about this book?")
@@ -15,7 +19,7 @@ def random_book(books, number_of_books):
     return _index
 
 def get_book_list():
-    r  = requests.get("http://themorningnews.org/article/the-year-in-fiction-2019")
+    r  = requests.get(library_uri)
     data = r.text
     soup = BeautifulSoup(data, features="html.parser")
     soupy_books = soup.find_all('h2')
@@ -24,34 +28,34 @@ def get_book_list():
         if book and book.em:
             url = book.a['href']
             title_and_author = book.text
-            books.append({"title": title_and_author, "url": url})
+            books.append({ "title": title_and_author, "url": url })
     return books
 
 def run_prompts():
     books = get_book_list()
     number_of_books = len(books)
-    print("There are " + str(number_of_books) + " books to choose from.")
+    if number_of_books < 1:
+        print("Uh oh, no books to choose from!")
+        return
+    print("There are %s books to choose from." % number_of_books)
     print("Would you like to choose a book? (y/n): ")
     chosen_input = input()
     if chosen_input == 'n':
         print("OK, goodbye.")
     if chosen_input == 'y':
-        choosing_a_book = True
-        while choosing_a_book == True:
-            selected_book = random_book(books, number_of_books)
-            book_chosen = input()
-            if book_chosen == 'y':
+        selected_book = -1
+        while True:
+            selected_book = random_book(books, number_of_books, selected_book)
+            book_response = input()
+            if book_response == 'y':
                 print("Hooray! Enjoy reading " + books[selected_book]['title'])
-                choosing_a_book = False
-            if book_chosen == 'ar':
+                break
+            if book_response == 'ar':
                 books.pop(selected_book)
                 number_of_books = number_of_books = len(books)
-                print("OK, I've removed that book from your library. There are now " + str(number_of_books) + " books to choose from.")
+                print("OK, I've removed that book from your library. There are now %s books to choose from." % number_of_books)
+            if book_response == 'exit':
+                break
 
 if __name__ == "__main__":
     run_prompts()
-
-
-
-
-
